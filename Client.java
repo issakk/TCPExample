3019270cefb659ad0716c7687aff0e73f092c4bd
chill <issakk@126.com>
2019-11-01 13:54:53 +0800

    第一版 服务器端直接接收 没用while

---
+++ Client.java
@@ -0,0 +1,44 @@
+package homework.homewok10;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Arrays;
+
+public class Client{
+    public static void main(String[] args) throws IOException{
+        Socket ss = new Socket("127.0.0.1", 8888);
+        InputStream is = ss.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+
+        BufferedReader br = new BufferedReader(isr);
+
+        System.out.println(br.readLine());
+/////////////////////////////////////////////////////////////////////
+        OutputStream os = ss.getOutputStream();
+        BufferedOutputStream bos = new BufferedOutputStream(os);
+        File file = new File("2.png");
+        String name = file.getName().split("\\.")[1];
+        name = name + "" + name.length();
+        byte[] nameArray = name.getBytes();
+        System.out.println("name = " + name);
+        int len = 0;
+        int newLen = 0;
+        byte[] bytearray = new byte[1024 * 8];
+        FileInputStream fis = new FileInputStream(file);
+        BufferedInputStream bis = new BufferedInputStream(fis);
+        while ((len = bis.read(bytearray)) != -1) {
+            newLen = len;
+        }
+        byte[] newArray = new byte[newLen + nameArray.length];
+        System.out.println("newArray.length = " + newArray.length);
+        System.out.println("nameArray = " + nameArray.length);
+        System.arraycopy(bytearray, 0, newArray, 0, newLen);
+        System.out.println("Arrays.toString(newArray) = " + Arrays.toString(newArray));
+        System.arraycopy(nameArray, 0, newArray, newArray.length - nameArray.length, nameArray.length);
+        System.out.println("Arrays.toString(newArray) = " + Arrays.toString(newArray));
+
+        bos.write(newArray);
+        bos.flush();
+
+    }
+}
---
+++ DownloadServer.java
@@ -0,0 +1,71 @@
+package homework.homewok10;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+public class DownloadServer{
+    public static void main(String[] args) throws IOException{
+        ServerSocket serverSocket = new ServerSocket(8888);
+
+        while (true) {
+            Socket accept = serverSocket.accept();//等待连接 有反应就是连接上
+            new Thread(() -> {
+                String hostAddress = accept.getInetAddress().getHostAddress();
+                String name = Thread.currentThread().getName();
+                try {
+                    OutputStream os = accept.getOutputStream();
+                    OutputStreamWriter osw = new OutputStreamWriter(os);
+
+                    BufferedWriter bw = new BufferedWriter(osw);
+
+                    DateFormat instance = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                    String format = instance.format(new Date());
+                    System.out.println(hostAddress + " 登录 " + format + " 线程: " + name);
+                    bw.write("欢迎登录 " + format);
+                    bw.newLine();
+                    bw.flush();
+
+                    InputStream is = accept.getInputStream();
+                    BufferedInputStream bis = new BufferedInputStream(is);
+                    int len = 0;
+                    int newLen = 0;
+                    byte[] allArray = new byte[1024 * 8];
+                    //len = bis.read(allArray);
+                    System.out.println("len = " + len);
+//                    while ((len = bis.read(allArray)) != -1) {
+//                        newLen = len;
+//                    }
+                    System.out.println("Arrays.toString(allArray) = " + Arrays.toString(allArray));
+                    byte[] newArray = new byte[len];
+                    System.arraycopy(allArray, 0, newArray, 0, len);
+                    char temp = (char) newArray[newArray.length - 1];
+                    int num = Integer.parseInt(String.valueOf(temp));
+                    System.out.println("num = " + num);
+                    String type = new String(newArray, newArray.length - num - 1, num);
+                    System.out.println("type = " + type);
+                    byte[] outArray = new byte[len - num - 1];
+                    System.arraycopy(newArray, 0, outArray, 0, outArray.length);
+                    System.out.println("Arrays.toString(outArray) = " + Arrays.toString(outArray));
+
+                    FileOutputStream fos = new FileOutputStream(System.currentTimeMillis() + "." + type);
+                    fos.write(outArray);
+                    fos.close();
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }).start();
+
+
+        }
+
+    }
+}
